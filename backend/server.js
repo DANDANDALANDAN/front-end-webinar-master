19fd0a433e07fc712b6bcf791190f6b2d10feed9
DANDANDALANDAN <sulitdanny1995@gmail.com>
2024-02-10 15:32:06 +0800

    added routes

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,20 @@
+const express = require ("express");
+const bodyParser = require ("body-parser");
+
+const app = express ();
+
+/* Middleware */
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended:true }));
+
+/* Routes */
+const indexRouter = require('./routes/index');
+app.use('/', indexRouter);
+
+const PORT = 3001;
+
+app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}`);
+});
+
 console.log("TitoDev");
